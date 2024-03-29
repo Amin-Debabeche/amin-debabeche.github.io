59a9ad8e1765518ccc7a4848236416a466e086e0
Amin-Debabeche <amin.debabeche@epfl.ch>
2024-03-29 14:30:42 +0100

    first content

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,26 +20,26 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Amin Debabeche",
+  title: "Hi all, I'm Amin",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Drug Discovery Researcher 🚀 having an experience of building research pipeline using MD / Biostatistics / Quantum Dynamics / Machine Learning and any cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/16GFZH570H_O9iBYeBPlAG2d8ZwFOovD8/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/amin-debabeche",
+  linkedin: "https://www.linkedin.com/in/amin-debabeche/",
+  gmail: "debabeche.amin@gmail.com",
+  // gitlab: "https://gitlab.com/saadpasta",
+  // facebook: "https://www.facebook.com/saad.pasta7",
+  // medium: "https://medium.com/@saadpasta",
+  // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -49,14 +49,14 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "LAZY PRODUCT MANAGER WHO WANTS TO AUTOMATE EVERYTHING",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Manage developer teams to create an high-end product"
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ Communicate with stakeholders to understand their needs"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Leads the software delivery, support and maintenance"
     )
   ],
 
@@ -89,10 +89,6 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-node"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
       skillName: "npm",
       fontAwesomeClassname: "fab fa-npm"
     },
@@ -103,10 +99,6 @@ https://fontawesome.com/icons?d=gallery */
     {
       skillName: "aws",
       fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
     },
     {
       skillName: "python",
@@ -126,23 +118,48 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "Massachusetts Institute of Technology",
+      logo: require("./assets/images/mit.png"),
+      subHeader: "Visiting Researcher",
+      duration: "April 2023 - December 2023",
+      desc: "Participated in the research of bringing computational tools to WDR5 peptide binder discovery and published 1 Intellectual Property.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Using docking, molecular dynamics together with statistical analysis to validate low- fidelity experimental data, and understand the binding mechanism of potential cancer drugs.",
+        "Fine-tuning results with foundation models.",
+        "Developped a deep learning model to generate new molecules."
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "Ecole Polytechnique Fédéral de Lausanne",
+      logo: require("./assets/images/epfl.png"),
+      subHeader: "MSc. Molecular and Biological Chemistry",
+      duration: "September 2020 - December 2023",
+      desc: "A chemist that hates labs and prefer to use computational tools to understand the world.",
+      descBullets: [
+        "Computational methods in molecular quantum mechanics, Understanding advanced molecular simulation, Optical methods in chemistry, Advanced photochemistry, Risk management, Food biotechnology",
+        "Research project on double perovskite design using Deep Learning at the GMF Lab",
+        "Teaching Assistant in advanced chemistry & global issues"
+      ]
+    },
+    {
+      schoolName: "Ecole Polytechnique Fédéral de Lausanne",
+      logo: require("./assets/images/epfl.png"),
+      subHeader: "Minor. Financial Engineering",
+      duration: "September 2020 - December 2023",
+      desc: "Focus on ML and big data for finance as well as Blockchain and Investments solutions.",
+      descBullets: [
+        "WallStreetBets: Evaluating the ability of public sentiment to influence financial markets.",
+        "Machine learning for finance, Financial big data, Blockchains and distributed ledgers, Investments, Quantitative risk management, Introduction to Corporate finance, Stochastic calculus.",
+      ]
+    },
+    {
+      schoolName: "Ecole Polytechnique Fédéral de Lausanne",
+      logo: require("./assets/images/epfl.png"),
+      subHeader: "Bachelor in Theoretical chemistry",
+      duration: "September 2017 - August 2020",
+      desc: "Focus on physics, computer simulation, machine learning and math.",
+      descBullets: [
+      ]
     }
   ]
 };
@@ -153,16 +170,20 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      Stack: "Programming", //Insert stack or technology you have experience in
+      progressPercentage: "100%" //Insert relative proficiency in percentage
     },
     {
-      Stack: "Backend",
+      Stack: "Biostatistics",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Machine Learning",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Project Management",
       progressPercentage: "70%"
-    },
-    {
-      Stack: "Programming",
-      progressPercentage: "60%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -174,29 +195,39 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Product Manager",
+      company: "Solutions & Funds",
+      companylogo: require("./assets/images/suf.png"),
+      date: "Oct 2020 – Aug 2022",
+      desc: "Design, development, and maintenance of tools to automate manual processes and to bridge the company’s needs..",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Business process flow analysis together with operational project management",
+        "Introduction of new technologies in the Operations department and administration of databases",
+        "Participation in the company’s digitalization strategy (Head of Innovation Committee)"
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Machine Learning Researcher",
+      company: "IBM Research Europe",
+      companylogo: require("./assets/images/ibm.png"),
+      date: "Aug 2021 – Feb 2022",
+      desc: "Standardizing chemical compounds with language models.",
+      descBullets: [
+        "Transformer-based model for a translational task with 98% accuracy and transferability to catalyst dataset 80.12%",
+        "Databases scraping,cleaning,and automatic labelling",
+        "Machine learning applied to chemistry using Natural Language Processing"
+      ]
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Sales Manager",
+      company: "Voggt",
+      companylogo: require("./assets/images/voggt.png"),
+      date: "Dec 2022 – Mar 2023",
+      desc: "",
+      descBullets: [
+        "Scaling the French market with a general 150% KPls growth and achievement of commercial targets",
+        "Creating an auto-insurance script predicting market movement and offering financial products to clients to back up their assets",
+      ]
     }
   ]
 };
@@ -239,7 +270,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
@@ -300,7 +331,7 @@ const achievementSection = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -324,7 +355,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -343,7 +374,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,22 +387,22 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+41794215022",
+  email_address: "debabeche.amin@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
